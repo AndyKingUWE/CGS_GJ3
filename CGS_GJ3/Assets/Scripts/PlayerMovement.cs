9c8e014a5728b43f79c386a8847a2ad71e6fda4c
Matthew <Matthew2.Churchyard@live.uwe.ac.uk>
2018-11-16 18:07:52 +0000

    Created Basic Spawning for Tracks

--- CGS_GJ3/Assets/Scripts/PlayerMovement.cs
+++ CGS_GJ3/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody movingObject;
-	public Vector3 movingSpeed = new Vector3(3,0,0);
+	public Vector3 movingSpeed = new Vector3(0,0,5);
 
 	// Use this for initialization
 	void Start () {
