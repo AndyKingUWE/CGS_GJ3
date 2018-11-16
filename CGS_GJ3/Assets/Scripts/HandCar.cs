34a0c1f55ba9ed481fd3d57babffebcd1da8bd5a
med1337 <leszek.bagnucki@gmail.com>
2018-11-16 20:50:14 +0000

    Handcar vol2 (notworking)

--- CGS_GJ3/Assets/Scripts/HandCar.cs
+++ CGS_GJ3/Assets/Scripts/HandCar.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pixelplacement;
 
 public class HandCar : MonoBehaviour
 {
@@ -12,16 +13,21 @@ public class HandCar : MonoBehaviour
         UP = 1,
         DOWN = 2
     }
-
+    [SerializeField]
+    private Track currentTrack;
     [SerializeField] private float speedModifier =5;
     [SerializeField] private Transform movementLever;
     [SerializeField]
     private List<WheelCollider> motorWheels;
+    [SerializeField] private List<BoxCollider> triggers;
     private bool coroutineStarted = false;
-    private int modifier = 20;
+    private float modifier = 20;
     private PumpState pumpState = PumpState.IDLE;
     private float timer = 0.0f;
     private Rigidbody myRigidbody;
+    [SerializeField] private Transform axle;
+    [SerializeField] private Transform axle2;
+    private float ClosestPoint;
     // Use this for initialization
     void Start()
     {
@@ -29,10 +35,35 @@ public class HandCar : MonoBehaviour
         myRigidbody.centerOfMass = Vector3.down;
     }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        //if (other.collider.tag == "Tracks")
+        //{
+        //    myRigidbody.AddForce(-other.relativeVelocity);
+        //    Debug.Log(other.relativeVelocity);
+        //}
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+        int layerMask = 1 << LayerMask.NameToLayer("Tracks");
+        //layerMask = ~layerMask;
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position + transform.up*10, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position + transform.up * 10, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+            var prevTrack = currentTrack;
+        
+            currentTrack = hit.collider.gameObject.GetComponent<Track>();
+            if(currentTrack!=prevTrack)
+            {
+                ResetPosition();
+            }
+        }
 
+        
         if (Input.GetKeyDown(KeyCode.KeypadPlus))
         {
             speedModifier++;
@@ -50,13 +81,53 @@ public class HandCar : MonoBehaviour
         {
             AddForce(1);
         }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Brake(1);
+        }
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            Brake(0);
+
+        }
+        if(modifier>20)
+        {
+            AddForce(modifier * Time.unscaledDeltaTime);
+        }
     }
 
-    private void OnDrawGizmos()
+    private void LateUpdate()
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(myRigidbody.worldCenterOfMass, 0.01f);
+        //ResetPosition();
+        //if(myRigidbody.velocity.magnitude<3)
+        //{
+        //    myRigidbody.
+        //}
     }
+
+    internal void SetPosition()
+    {
+        var newPos = currentTrack.spline.GetPosition(ClosestPoint, true, 100);
+        newPos.y = transform.position.y;
+        myRigidbody.MovePosition(newPos);
+        myRigidbody.MoveRotation(Quaternion.Euler(0, 0, 0));
+    }
+
+    internal void ResetPosition()
+    {
+        ClosestPoint = currentTrack.spline.ClosestPoint(transform.position);
+        var newPos = transform.position;
+        newPos.x = currentTrack.spline.GetPosition(ClosestPoint,true,10000).x;
+        newPos.z = currentTrack.spline.GetPosition(ClosestPoint, true, 10000).z;
+        myRigidbody.MovePosition(newPos);
+        myRigidbody.MoveRotation(Quaternion.Euler(0, 0, 0));
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawSphere(myRigidbody.worldCenterOfMass, 0.01f);
+    //}
 
     private IEnumerator WaitForInput(bool positive)
     {
@@ -78,8 +149,12 @@ public class HandCar : MonoBehaviour
                 if (Input.GetAxis("Vertical") > 0)
                 {
                     Debug.Log("nice one!");
-                    modifier += 10;
+                    modifier ++;
                     coroutineStarted = false;
+                    if(modifier>100)
+                    {
+                        modifier = 100;
+                    }
 
                     yield break;
                 }
@@ -89,7 +164,11 @@ public class HandCar : MonoBehaviour
                 if (Input.GetAxis("Vertical") < 0)
                 {
                     Debug.Log("nice one!");
-                    modifier += 10;
+                    modifier ++;
+                    if (modifier > 100)
+                    {
+                        modifier = 100;
+                    }
                     coroutineStarted = false;
                     yield break;
                 }
@@ -100,16 +179,33 @@ public class HandCar : MonoBehaviour
 
         }
 
-        modifier = 10;
+        modifier = 20;
+    }
+
+    private void Brake(float force)
+    {
+        foreach (var wheel in motorWheels)
+        {
+            wheel.brakeTorque = force * speedModifier;
+        }
     }
 
     private void AddForce(float force)
     {
+        ClosestPoint +=  Time.unscaledDeltaTime;
+        if(ClosestPoint>1)
+        {
+            ClosestPoint = 1 - ClosestPoint;
+            currentTrack = currentTrack.forwardTrack;
+        }
+        SetPosition();
         Debug.Log(force * speedModifier);
         //GetComponent<Rigidbody>().AddForce(transform.forward * force * speedModifier);
+        //axle.Rotate(Vector3.right * force* speedModifier);
+        //axle2.Rotate(Vector3.right * force* speedModifier);
         foreach (var wheel in motorWheels)
         {
-            wheel.motorTorque = force * speedModifier;
+            //wheel.motorTorque = force * speedModifier;
         }
     }
 
@@ -124,8 +220,8 @@ public class HandCar : MonoBehaviour
             else
             {
                 var force = Input.GetAxis("Vertical") * Time.unscaledDeltaTime * modifier;
+                modifier += Mathf.Abs( force);
                 movementLever.Rotate(Vector3.right, force);
-                AddForce(Mathf.Abs(force));
                 timer = 0.0f;
             }
             if (timer >= 1.0f)
@@ -144,7 +240,7 @@ public class HandCar : MonoBehaviour
             if (movementLever.localEulerAngles.x > 15)
                 movementLever.localRotation = Quaternion.Euler(14.99f, 0, 0);
             //TODO: UI
-            Debug.Log("GO DOWN");
+            //Debug.Log("GO DOWN");
         }
         if (movementLever.localEulerAngles.x < 350 && movementLever.localEulerAngles.x > 180)
         {
@@ -155,7 +251,7 @@ public class HandCar : MonoBehaviour
                 StartCoroutine(WaitForInput(true));
 
             //TODO: UI
-            Debug.Log("GO UP");
+            //Debug.Log("GO UP");
             //movementLever.localRotation.SetEulerAngles(14.99f, 0, 0);
         }
         //if (movementLever.localEulerAngles.x < 165)
