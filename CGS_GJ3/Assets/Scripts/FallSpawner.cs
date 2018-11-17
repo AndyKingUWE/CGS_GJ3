7111be0724373bfbd9a78f8fb4ac4ab346e39a01
Charlie Saunders <charlie2.saunders@live.uwe.ac.uk>
2018-11-17 15:45:19 +0000

    Added 3rd party assets

--- CGS_GJ3/Assets/Scripts/FallSpawner.cs
+++ CGS_GJ3/Assets/Scripts/FallSpawner.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class FallSpawner : MonoBehaviour {
 
-	Transform childToFall;
-	Transform parentLocation;
+	//Transform childToFall;
 	public Vector3 startPos;
 	public Vector3 endPos;
-	public float fallDistance;
-	bool triggered = false;
+    public float speed; 
+	float fallDistance; 
+	bool triggered = true;
 
 	// Use this for initialization
 	void Start()
 	{
-		parentLocation = transform;
-		childToFall = transform.GetChild(0);
-		startPos = new Vector3(parentLocation.localPosition.x, parentLocation.localPosition.y + 20, parentLocation.localPosition.z);
-		childToFall.transform.position = startPos;
-		endPos = parentLocation.position;
-
+        //childToFall = transform.GetChild(0);
+        endPos = transform.position;
+        startPos = new Vector3(transform.localPosition.x, transform.localPosition.y + 20, transform.localPosition.z);
+		transform.position = startPos;
+        fallDistance = Vector3.Distance(startPos, endPos); 
 	}
 
 	// Update is called once per frame
@@ -27,7 +26,12 @@ public class FallSpawner : MonoBehaviour {
 	{
 		if (triggered)
 		{
-			childToFall.transform.position = Vector3.Lerp(startPos, endPos, fallDistance);
+            //Distance moved = time * speed 
+            float distCovered = Time.time * speed;
+            //Fraction of journey completed
+            float fracJourney = distCovered / fallDistance; 
+            //Lerp by fraction of distance covered
+			transform.position = Vector3.Lerp(startPos, endPos, fracJourney);
 		}
 	}
 
