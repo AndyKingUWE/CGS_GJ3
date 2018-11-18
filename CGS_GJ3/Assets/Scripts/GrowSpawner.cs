ea221fb08e7e9b2bdccf349f5a2945af894a64a5
Matthew <Matthew2.Churchyard@live.uwe.ac.uk>
2018-11-18 15:13:26 +0000

    Added Tree Spawning, Animation and Leaf Effect

--- CGS_GJ3/Assets/Scripts/GrowSpawner.cs
+++ CGS_GJ3/Assets/Scripts/GrowSpawner.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class GrowSpawner : MonoBehaviour {
 
-	Transform objectToGrow;
+	Renderer[] treeRend;
+	Renderer renderTree;
+	ParticleSystem leaves;
 
 	// Use this for initialization
 	void Start () {
-
+		treeRend = GetComponentsInChildren<Renderer>();
+		foreach (Renderer renderTree in treeRend)
+		{
+			renderTree.enabled = false;
+		}
+		leaves = GetComponentInChildren<ParticleSystem>();
+		leaves.Stop();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	 void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "SpawnTrigger") {
+
+			foreach (Renderer renderTree in treeRend)
+			{
+				renderTree.enabled = true;
+			}
+
+			GetComponentInChildren<Animator>().SetBool("GrowTriggered", true);
+			leaves.Play();
+
+		}
 	}
 }
