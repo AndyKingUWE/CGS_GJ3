1796cafdcafb01c98ad73bac50bad584130f9ea2
Elliott Smith <elliottsmith1@hotmail.co.uk>
2018-11-22 20:39:44 +0000

    dynamite update
    -edited fuse and explosion particles
    -added fuse and explosion particles
    -fuse lights when picked up

--- CGS_GJ3/Assets/dynamite.cs
+++ CGS_GJ3/Assets/dynamite.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class dynamite : MonoBehaviour {
 
     private bool lit = false;
     private float fuse_timer = 0.0f;
+    private float fuse_spark_speed = 0.0f;
 
     [SerializeField] float fuse_length = 5.0f;
 
     [SerializeField] ParticleSystem explosion_prefab;
     [SerializeField] ParticleSystem fire_prefab;
     [SerializeField] ParticleSystem sparks;
-    //[SerializeField] VRTK_InteractableObject linkedObject;
+    [SerializeField] VRTK_InteractableObject linkedObject;
+    [SerializeField] Transform fuse_start_pos;
+    [SerializeField] Transform fuse_end_pos;
+    
 
     // Use this for initialization
     void Start () {
@@ -22,9 +27,18 @@ public class dynamite : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if (linkedObject.IsGrabbed() && !lit)
+        {
+            LightFuse();
+        }
+
 		if (lit)
         {
             fuse_timer += Time.deltaTime;
+
+            fuse_spark_speed += Time.deltaTime / fuse_length;
+
+            sparks.gameObject.transform.position = Vector3.Lerp(fuse_start_pos.position, fuse_end_pos.position, fuse_spark_speed);
 
             if (fuse_timer > fuse_length)
             {
@@ -42,12 +56,6 @@ public class dynamite : MonoBehaviour {
     private void Explode()
     {
         Instantiate(explosion_prefab, transform.position, transform.rotation);
-        Instantiate(fire_prefab, transform.position, transform.rotation);
         Destroy(this.gameObject);
     }
-
-    //protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
-    //{
-    //    FireProjectile();
-    //}
 }
