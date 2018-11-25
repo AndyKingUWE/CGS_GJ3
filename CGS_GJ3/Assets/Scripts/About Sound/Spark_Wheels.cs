e012ef99a3b72d0917f21b5ba8d10b21c7a5c45e
wangw3939 <44263751+wangw3939@users.noreply.github.com>
2018-11-25 20:11:40 +0000

    Collected all the sound effects needed at present

---
+++ CGS_GJ3/Assets/Scripts/About Sound/Spark_Wheels.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spark_Wheels : MonoBehaviour {
+    public AudioSource AS;
+
+    public float speed;
+    private bool chang = true;
+    private void handcarVoiceControl()
+    {
+        AS.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
