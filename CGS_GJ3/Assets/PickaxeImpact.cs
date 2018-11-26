73c53a8a920e959c8ba13a65308efe6235c79f4b
Elliott Smith <elliottsmith1@hotmail.co.uk>
2018-11-26 19:27:18 +0000

    Pickaxe sparks when hitting something

---
+++ CGS_GJ3/Assets/PickaxeImpact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickaxeImpact : MonoBehaviour {
+
+    [SerializeField] ParticleSystem impact_sparks_prefab;
+    [SerializeField] Transform impact_end_pos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Vector3 pos = col.contacts[0].point;
+
+        Debug.Log(Vector3.Distance(pos, impact_end_pos.position));
+
+        if (Vector3.Distance(pos, impact_end_pos.position) < 0.5f)
+        {
+            if (col.impulse.magnitude > 10.0f)
+            {
+                ParticleSystem obj = Instantiate(impact_sparks_prefab, pos, transform.rotation);
+
+                Destroy(obj, 1.0f);
+            }
+        }
+    }
+}
